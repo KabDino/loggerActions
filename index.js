7dacfa2e874c4bd6db9d46d6a9cbccd5d4a333a5
KabDino <34832184+KabDino@users.noreply.github.com>
2022-06-02 22:47:41 +0300

    Fixed code-refactoring (#2)
    Co-authored-by: Denis Kabachuk <d.kabachuk@wowmaking.net>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const initialState = { actions: [] };
 const SET_ACTION = 'LOGGER_ACTIONS/SET_ACTION';
 
+let wasShownError = false;
+
 export const loggerActions = (state = initialState, action) => {
   if (action.type === SET_ACTION) {
     return { ...state, actions: [...state.actions, action.payload] };
@@ -8,23 +10,22 @@ export const loggerActions = (state = initialState, action) => {
   return state;
 };
 
-export const loggerActionsMiddleWare = exceptions => ({ dispatch }) => next => action => {
-    const date = new Date();
-    const time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-    let EXCEPTION_LIST = [SET_ACTION];
-    
-    if (typeof exceptions !== 'undefined' && !Array.isArray(exceptions)) {
-      console.error('ExceptionsList for customMiddleWare must be an array of strings');
-    } else {
-      EXCEPTION_LIST = EXCEPTION_LIST.concat(exceptions);
-    }
+export const loggerActionsMiddleware = exceptions => ({ dispatch }) => next => action => {
+  if (typeof exceptions !== 'undefined' && !Array.isArray(exceptions) && !wasShownError) {
+    console.warn('[ATTENTION] Exceptions for loggerActionsMiddleware must be an array of strings!');
+    wasShownError = true;
+  }
 
-    if (action.type && !EXCEPTION_LIST.includes(action.type)) {
-      dispatch({
-        type: SET_ACTION,
-        payload: { [action.type]: { time, payload: action.payload } },
-      });
-    }
+  const exceptionsList = exceptions.length ? [SET_ACTION, ...exceptions] : [SET_ACTION];
+  const date = new Date();
+  const time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-    next(action);
-  };
+  if (action.type && !exceptionsList.includes(action.type)) {
+    dispatch({
+      type: SET_ACTION,
+      payload: { [action.type]: { time, payload: action.payload } },
+    });
+  }
+
+  next(action);
+};
